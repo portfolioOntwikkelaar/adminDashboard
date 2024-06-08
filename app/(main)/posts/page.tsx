b187be4498336cae8b070b8ae92e3367cf5d6b57
WebDeveloper <58941189+portfolioOntwikkelaar@users.noreply.github.com>
2024-06-08 19:55:45 +0200

    1955
    8jun

---
+++ app/(main)/posts/page.tsx
@@ -0,0 +1,14 @@
+import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
+import PostsTable from "@/components/posts/PostsTable"
+
+
+const PostsPage = () => {
+  return <>
+  <BackButton text='Go Back' link='/' />
+    <PostsTable></PostsTable>
+    <PostsPagination />
+  </>;
+
+}
+export default PostsPage;
